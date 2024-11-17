39713a09d9fe97790c704283dbd25a2a7091d8cb
HrithikSawant <hrithiksawant80@gmail.com>
2024-11-17 17:29:40 +0530

    Methods Examples

--- 16methods/main.go
+++ 16methods/main.go
@@ -1,20 +1,9 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Structs in golang")
-	// no inheritance in golang; No super or parent
-
-	hrithik := User{"Hrithik", "hrithik@go.dev", true, 18}
-	fmt.Println(hrithik)
-	fmt.Printf("hrithik details are: %+v\n", hrithik)
-	fmt.Printf("Name is %v and email is %v\n", hrithik.Name, hrithik.Email)
-
-	hrithik.GetStatus()
-	hrithik.NewMail()
-	fmt.Printf("Name is %v and email is %v\n", hrithik.Name, hrithik.Email)
-}
+import (
+	"fmt"
+	"math"
+)
 
 type User struct {
 	Name   string
@@ -30,4 +19,52 @@ func (u User) GetStatus() {
 func (u User) NewMail() {
 	u.Email = "testing@go.dev"
 	fmt.Println("Email of this user is:", u.Email)
+}
+
+// Example: 1 Method
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// Methods Not Struct Type
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+// Regular Abs() with no change in functionality
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	fmt.Println("Structs in golang")
+	// no inheritance in golang; No super or parent
+
+	hrithik := User{"Hrithik", "hrithik@go.dev", true, 18}
+	fmt.Println(hrithik)
+	fmt.Printf("hrithik details are: %+v\n", hrithik)
+	fmt.Printf("Name is %v and email is %v\n", hrithik.Name, hrithik.Email)
+
+	hrithik.GetStatus()
+	hrithik.NewMail()
+	fmt.Printf("Name is %v and email is %v\n", hrithik.Name, hrithik.Email)
+
+	// Example1: Methods
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	fmt.Println(Abs(v))
+
+	// Method Not Struct Type
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
